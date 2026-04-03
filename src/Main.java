872d460ee3f6c1223a6109871cda45e27d63f522
caua <caua.chagashd@gmail.com>
2026-04-03 19:41:31 -0300

    Identificação teste

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String []args) {
-        int x = 5;
-        int y = 10;
-        int z = 15;
-        System.out.print(x + y + z);
+        String EstudanteNome = "Carlos";
+        int EstudanteID = 15;
+        int EstudanteIdade = 23;
+        float EstudanteTaxa = 75.25f;
+        char EstudanteNota = 'B';
+
+        System.out.println ("Nome: " + EstudanteNome);
+        System.out.println ("Idade: " + EstudanteIdade);
+        System.out.println ("ID: " + EstudanteID);
+        System.out.println ("Taxa Paga: " + EstudanteTaxa);
+        System.out.println ("Nota: " + EstudanteNota);
     }
 }
